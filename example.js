75507f04846d6eae9425fea18b688ed9bcbb89f8
Luka Bla9eecki <luka.blazecki@infobip.com>
2013-03-28 15:58:21 +0100

    Added module

---
+++ example.js
@@ -0,0 +1,30 @@
+var token1 = "11111a960806fcfa8f4abc73e015b7a730118c783a6dcede5dda9f34e6fe9270";
+var token2 = "11111a960806fcfa8f4abc73e015b7a730118c783a6dcede5dda9f34e6fe9270";
+
+
+var certData = "-----BEGIN CERTIFICATE-----\n\n-----END CERTIFICATE-----";
+var keyData =  "-----BEGIN RSA PRIVATE KEY-----\n\n-----END RSA PRIVATE KEY-----";
+var apnsMessage1 = {
+    expiry :  Math.floor(Date.now() / 1000) + 300,
+    _id : "1asd1231"
+};
+var apnsMessage2 = {
+    _id : "1asd1231"
+};
+var objectCert = {
+    certData : certData,
+    keyData : keyData
+};
+apnsMessage1.payload = apnsMessage2.payload = {
+    'aps': {"badge" : "123", "alert" : "Test it bulk", "sound" : "default"},
+    'type': "text/html",
+    'mimeType': "text/html",
+    'messageID' : "1asd1231"
+};
+
+var sender = new (require('./iOSSender')).SenderApns(objectCert, true);
+sender.sendThroughApns([apnsMessage1, apnsMessage2], [token1, token2], function (array) {
+    console.log(array);
+}, function (error) {
+    console.log(error);
+});
---
+++ iOSSender.js
@@ -0,0 +1,203 @@
+var tls = require('tls');
+var _ = require('underscore');
+
+function SenderApns(objectCert, production) {
+
+    this.host = 'gateway.push.apple.com';
+    this.objectCert = objectCert;
+    this.resultArray = [];
+    this.callsuccess = null;
+    this.callsuccess = null;
+    this.notifications = null;
+    this.reconnectTry = 0;
+    this.tokens = [];
+    this.tlsStream = null;
+
+    if (!production) {
+        this.host = 'gateway.sandbox.push.apple.com';
+    }
+
+}
+
+SenderApns.prototype.sendThroughApns = function (notifications, tokens, callsuccess, callerror) {
+
+    var self = this;
+    self.notifications = notifications;
+    self.tokens = tokens;
+    self.callsuccess = callsuccess;
+    self.callerror = callerror;
+    if (!this.isInputValid()) {
+        return;
+    }
+
+    //set last notification to fail
+    notifications.push({expiry : 30000, payload : {}, _id : "007"});
+    tokens.push("aaa111");
+
+    self.newConnection(function (tlsStream) {
+        self.tlsStream = tlsStream;
+        _.each(notifications, function (notification, index) {
+            tlsStream.write(makeApnsMessage(tokens[index], index, notification));
+        });
+    });
+};
+
+SenderApns.prototype.reSendThroughApns = function (notifications, tokens) {
+
+    var self = this;
+    self.notifications = notifications;
+    self.tokens = tokens;
+
+    self.newConnection(function (tlsStream) {
+        self.tlsStream = tlsStream;
+        _.each(notifications, function (notification, index) {
+            tlsStream.write(makeApnsMessage(tokens[index], index, notification));
+        });
+    });
+};
+
+
+SenderApns.prototype.newConnection = function (callsuccess) {
+
+    var self = this;
+    var tlsConnectionOptions = {
+        port : 2195,
+        host : self.host,
+        key : self.objectCert.keyData,
+        cert : self.objectCert.certData
+    };
+
+    try {
+        var tlsStream = tls.connect(tlsConnectionOptions, function () {
+            callsuccess(tlsStream);
+        });
+    } catch (err) {
+        self.callerror(err);
+        return;
+    }
+
+    tlsStream.on("error", function () {
+        self.reconnect();
+    });
+
+    tlsStream.on("data", function (data) {
+        self.errorResponse(data);
+    });
+
+    //tlsStream.on("close", function () { console.log("close"); });
+    //tlsStream.on("end", function () { console.log("end"); });
+};
+
+SenderApns.prototype.errorResponse = function (data) {
+
+    var self = this;
+
+    if (data[0] === 8) {
+        var apnsError = data[1];
+        var identifier = data.readUInt32BE(2);
+        _.each(self.tokens, function (token, index) {
+            if (index < identifier) {
+                self.resultArray.push({token : token, status : 0, _id : self.notifications[index]._id});
+            }
+            if (index === identifier) {
+                self.resultArray.push({token : token, status : apnsError,  _id : self.notifications[index]._id});
+            }
+        });
+
+        if (self.tokens.length === identifier + 1) {
+            self.tlsStream.destroySoon();
+            self.callsuccess(self.resultArray);
+            return;
+        }
+
+        var tokens = self.tokens.slice(identifier + 1);
+        var notifications = self.notifications.slice(identifier + 1);
+        self.reSendThroughApns(notifications, tokens);
+    }
+};
+
+SenderApns.prototype.reconnect = function () {
+
+    var self = this;
+    self.reconnectTry += 1;
+    setTimeout(function () {
+        self.reSendThroughApns(self.notifications, self.tokens);
+    }, 1000 * self.reconnectTry * self.reconnectTry);
+};
+
+SenderApns.prototype.isInputValid = function () {
+
+    var isValid = true;
+    if (typeof (this.callerror) !== 'function' || typeof (this.callsuccess) !== 'function') {
+        return false;
+    }
+    if (!this.objectCert || !this.objectCert.certData || !this.objectCert.keyData) {
+        this.callerror("Object cert is not valid!");
+        return false;
+    }
+    if (!this.notifications || !this.tokens || this.tokens.length !== this.notifications.length) {
+        this.callerror("Notifications or tokens are not valid or not the same length!");
+        return false;
+    }
+    _.each(this.notifications, function (notification) {
+        if (!notification || !notification.payload || !notification._id || isNaN(notification.expiry)) {
+            isValid = false;
+        }
+    });
+    if (!isValid) {
+        this.callerror("Some notification is not valid!");
+        return false;
+    }
+    _.each(this.tokens, function (token) {
+        try {
+            var tempToken =  new Buffer(token.replace(/\s/g, ""), "hex");
+        } catch (error) {
+            isValid = false;
+            return;
+        }
+        if (tempToken.length !== 32) {
+            isValid = false;
+        }
+    });
+    if (!isValid) {
+        this.callerror("Some token is not valid!");
+        return false;
+    }
+
+    return true;
+};
+
+function makeApnsMessage(token, identifier, notification) {
+
+    token = new Buffer(token.replace(/\s/g, ""), "hex");
+    var encoding = 'utf8';
+    var message = JSON.stringify(notification.payload);
+    var messageLength = Buffer.byteLength(message, encoding);
+    var position = 0;
+    var apnsMessage = new Buffer(1 + 4 + 4 + 2 + token.length + 2 + messageLength);
+
+    // Command
+    apnsMessage[position] = 1;
+    position += 1;
+    // Identifier
+    apnsMessage.writeUInt32BE(identifier, position);
+    position += 4;
+    // Expiry
+    apnsMessage.writeUInt32BE(notification.expiry, position);
+    position += 4;
+
+    // Token Length
+    apnsMessage.writeUInt16BE(token.length, position);
+    position += 2;
+    // Device Token
+    token.copy(apnsMessage, position, 0);
+    position += token.length;
+    // Payload Length
+    apnsMessage.writeUInt16BE(messageLength, position);
+    position += 2;
+    //Payload
+    apnsMessage.write(message, position, encoding);
+    return apnsMessage;
+}
+
+exports.SenderApns   = SenderApns;
